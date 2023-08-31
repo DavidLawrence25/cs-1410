7489762811c15cd0bdef434462ed22bd54d3d3f1
DavidLawrence25 <25dlawrence@go.dsdmail.net>
2023-08-31 16:54:01 -0600

    Finished(?) Financial Goals Calculator

--- no_brainers/01_fin_goals_calc.cpp
+++ no_brainers/01_fin_goals_calc.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <math.h>
 #include <unordered_map>
 using namespace std;
 
@@ -11,7 +12,7 @@ int text_prompt(string prompt, unordered_map<string, int> options) {
 		cout << prompt;
 		cin >> option;
 
-		if (options.find(option) == options.end()) return options[option];
+		if (options.find(option) != options.end()) return options[option];
 
 		cout << "Invalid input. Please try again.\n";
 	}
@@ -44,20 +45,42 @@ int menu() {
 	);
 }
 
-int calculate_years() {
+void calculate_years() {
 	double investment = text_prompt("Enter Investment Amount: ");
-	const double interest_rate = text_prompt("Enter Interest Rate: ");
+	const double interest_rate = 0.01 * text_prompt("Enter Interest Rate: ");
 	const double goal = text_prompt("Enter Financial Goal: ");
 
-	int years;
+	int goal_width = (int)log10(goal) + 4;
+
+	int years = 0;
 	while (investment < goal) {
 		years++;
 		investment += investment * interest_rate;
-		cout << "Year " << setw(3) << years << ". "; // set width based on the goal
+		cout << "Year " << setw(3) << years << ". " << setw(goal_width)
+		<< fixed << setprecision(2) << investment << endl;
 	}
+
+	if (years == 1) cout << "That will take 1 year\n";
+	else cout << "That will take " << years << " years\n";
+}
+
+void calculate_amount() {
+	double investment = text_prompt("Enter Investment Amount: ");
+	const double interest_rate = 0.01 * text_prompt("Enter Interest Rate: ");
+	const int years = (int)text_prompt("Enter Years to Maturity: ");
+
+	investment *= pow(1 + interest_rate, years);
+
+	cout << "You will have " << investment << " in " << years << " years\n";
 }
 
 int main() {
+	cout << "Financial Goal Calculator\n";
 
-	return 0;
+	while (true) {
+		int choice = menu();
+		if (choice == 1) calculate_years();
+		else if (choice == 2) calculate_amount();
+		else if (choice == 3) return 0;
+	}
 }
