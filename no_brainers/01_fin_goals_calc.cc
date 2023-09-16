4bdafb6b0c2197b789b0c95fea045ee5acd67a1b
DavidLawrence25 <25dlawrence@go.dsdmail.net>
2023-09-15 18:53:47 -0600

    Fixed 01_fin_goals_calc.cc

--- no_brainers/01_fin_goals_calc.cc
+++ no_brainers/01_fin_goals_calc.cc
@@ -1,100 +1,94 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
 #include <math.h>
+
+#include <iomanip>
+#include <iostream>
+#include <string>
 #include <unordered_map>
 
-int menu_prompt(const std::string prompt, const std::string option_not_found_message, const std::unordered_map<std::string, int> options); // user_input.hpp
-double get_double(const std::string prompt, const std::string conversion_failed_message); // user_input.hpp
+#include "custom_libraries/user_input.h"
+
+// Prints the menu and returns the option the user picks.
 int menu();
+// Calculates the number of years until a goal is met and prints the result.
 void calculate_years();
+// Calculates the growth of an investment after a specified number of years
+// and prints the result.
 void calculate_amount();
 
 int main() {
-	std::cout << "Financial Goal Calculator\n";
+  std::cout << "Financial Goal Calculator\n";
 
-	while (true) {
-		int choice = menu();
-		if (choice == 1) {
-			calculate_years();
-		} else if (choice == 2) {
-			calculate_amount();
-		} else if (choice == 3) {
-			return 0;
-		}
-	}
+  while (true) {
+    switch (menu()) {
+      case 1:
+        calculate_years();
+        break;
+      case 2:
+        calculate_amount();
+        break;
+      case 3:
+      default:
+        return 0;
+    }
+  }
 }
 
-// borrowed from https://github.com/DavidLawrence25/cs-1410/blob/main/custom_libraries/user_input.hpp
-int menu_prompt(const std::string prompt, const std::string option_not_found_message, const std::unordered_map<std::string, int> options) {
-	while (true) {
-		std::string option;
-
-		std::cout << prompt;
-		std::cin >> option;
-
-		if (options.find(option) != options.end()) {
-			return options.at(option);
-		}
-
-		std::cout << option_not_found_message;
-	}
-}
-
-// borrowed from https://github.com/DavidLawrence25/cs-1410/blob/main/custom_libraries/user_input.hpp
-double get_double(const std::string prompt, const std::string conversion_failed_message) {
-	while (true) {
-		std::string input;
-
-		std::cout << prompt;
-		std::cin >> input;
-
-		try {
-			return stod(input);
-		} catch (const std::invalid_argument& ia) {
-			std::cout << conversion_failed_message;
-		}
-	}
-}
-
+// Prints the menu and returns the option the user picks.
+// Shorthand for a specific `rose::MenuPrompt` function call.
 int menu() {
-	const std::unordered_map<std::string, int> options {{"1", 1}, {"2", 2}, {"3", 3}};
-
-	return menu_prompt(
-		"1. Calculate Years to Financial Goal\n2. Calculate Amount Based on Years\n3. Quit\n",
-		"Invalid input. Please try again\n",
-		options
-	);
+  return rose::MenuPrompt(
+    "1. Calculate Years to Financial Goal\n2. Calculate Amount Based on Years\n3. Quit\n",
+    "Invalid input. Please try again\n",
+    {{"1", 1}, {"2", 2}, {"3", 3}}
+  );
 }
 
+// Calculates the number of years until a goal is met and prints the result.
+// After each year, prints the investment's value.
 void calculate_years() {
-	double investment = get_double("Enter Investment Amount: ", "Invalid input. Please try again\n");
-	const double interest_rate = 0.01 * get_double("Enter Interest Rate: ", "Invalid input. Please try again\n");
-	const double goal = get_double("Enter Financial Goal: ", "Invalid input. Please try again\n");
+  const std::string kConversionFailedMessage =
+      "Invalid input. Please try again\n";
 
-	const int goal_width = (int)log10(goal) + 4;
+  double investment = rose::GetDouble(
+      "Enter Investment Amount: ", kConversionFailedMessage);
+  const double kInterestRate = 0.01 * rose::GetDouble(
+      "Enter Interest Rate: ", kConversionFailedMessage);
+  const double kGoal = rose::GetDouble(
+      "Enter Financial Goal: ", kConversionFailedMessage);
 
-	int years = 0;
-	while (investment < goal) {
-		years++;
-		investment += investment * interest_rate;
-		std::cout << "Year " << std::setw(3) << years << ". " << std::setw(goal_width)
-		<< std::fixed << std::setprecision(2) << investment << '\n';
-	}
+  // There may be a better way to calculate the width to print at.
+  const int kGoalWidth = static_cast<int>(log10(kGoal)) + 4;
 
-	if (years == 1) {
-		std::cout << "That will take 1 year\n";
-	} else {
-		std::cout << "That will take " << years << " years\n";
-	}
+  int years = 0;
+  while (investment < kGoal) {
+    ++years;
+    investment += investment * kInterestRate;
+    std::cout << "Year " << std::setw(3) << years << ". " << std::setw(kGoalWidth)
+    << std::fixed << std::setprecision(2) << investment << '\n';
+  }
+
+  if (years == 1) {
+    std::cout << "That will take 1 year\n";
+  } else {
+    std::cout << "That will take " << years << " years\n";
+  }
 }
 
+// Calculates the growth of an investment after a specified number of years
+// and prints the result. Utilizes the following formula for compound interest:
+// A = P(1 + r/n)^(n*t)
+// Because this program only considers annually compounding interest, n = 1
+// A = P(1 + r)^t
 void calculate_amount() {
-	double investment = get_double("Enter Investment Amount: ", "Invalid input. Please try again\n");
-	const double interest_rate = 0.01 * get_double("Enter Interest Rate: ", "Invalid input. Please try again\n");
-	const int years = (int)get_double("Enter Years to Maturity: ", "Invalid input. Please try again\n");
+  double investment = rose::GetDouble(
+      "Enter Investment Amount: ", "Invalid input. Please try again\n");
+  const double kInterestRate = 0.01 * rose::GetDouble(
+      "Enter Interest Rate: ", "Invalid input. Please try again\n");
+  const int kYears = rose::GetInteger(
+      "Enter Years to Maturity: ", "Invalid input. Please try again\n");
 
-	investment *= pow(1 + interest_rate, years);
+  investment *= pow(1 + kInterestRate, kYears);
 
-	std::cout << "You will have " << investment << " in " << years << " years\n";
+  std::cout << "You will have $" << std::fixed << std::setprecision(2)
+  << investment << " in " << kYears << " years\n";
 }
