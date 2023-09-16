6bf18dd559af994c2c478494dc9bd44d965a0810
DavidLawrence25 <25dlawrence@go.dsdmail.net>
2023-09-16 08:00:27 -0600

    Added Documentation to 03_moving_grid.cc

--- labs/03_moving_grid.cc
+++ labs/03_moving_grid.cc
@@ -10,14 +10,19 @@ enum InputOptions {
   kQuit
 };
 
+// Prints the menu and returns the option the user picks.
 InputOptions GetOption();
 
+// Represents a point in the 2D cartesian coordinate system.
+// Would make more sense as a class, but for the purposes of
+// this assignment, it needs to be a struct.
 struct Point {
- public:
   int x;
   int y;
 
+  // Moves the point in a cardinal direction by one unit.
   void Move(InputOptions direction);
+  // Returns a string containing an ordered pair.
   std::string ToString();
 };
 
@@ -34,12 +39,16 @@ int main() {
   return 0;
 }
 
+// Prints the menu and returns the option the user picks.
+// Shorthand for a specific `rose::MenuPrompt` function call.
 InputOptions GetOption() {
   return rose::MenuPrompt<InputOptions>(
       "1. North\n2. South\n3. East\n4. West\n5. Quit\n", "Invalid input.\n",
       {{"1", kNorth}, {"2", kSouth}, {"3", kEast}, {"4", kWest}, {"5", kQuit}});
 }
 
+// Moves the point in a cardinal direction by one unit.
+// Assumes `direction` represents a cardinal direction.
 void Point::Move(InputOptions direction) {
   switch (direction) {
     case kNorth:
@@ -57,6 +66,8 @@ void Point::Move(InputOptions direction) {
   }
 }
 
+// Returns a string containing an ordered pair.
+// Results are formatted like this: "(x, y)"
 std::string Point::ToString() {
   std::string result = "(, )";
   result.insert(3, std::to_string(y));
