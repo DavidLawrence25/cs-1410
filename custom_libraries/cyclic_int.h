a0f7bf3af9d24dc63d325d9a1bca8e6543d9ffc8
DavidLawrence25 <25dlawrence@go.dsdmail.net>
2023-10-11 08:14:39 -0600

    Added Documentation to cyclic_int.h

--- custom_libraries/cyclic_int.h
+++ custom_libraries/cyclic_int.h
@@ -3,6 +3,11 @@
 
 namespace rose {
 
+// Represents an integer bound between a minimum and maximum value.
+// When incremented or decremented, the integer will behave as normal unless
+// it steps outside the bounds, at which point it will wrap around to the other
+// bound (i.e., going under the min will underflow to the max and going over the
+// max will overflow to the min).
 class CyclicInt {
  public:
   CyclicInt(int min, int max) {
@@ -26,12 +31,14 @@ class CyclicInt {
     return value_;
   }
 
+  // Increments `value_` by one, overflowing to the minimum value if necessary.
   CyclicInt &operator++() {
     ++value_;
     if (value_ > max_) value_ = min_;
     return *this;
   }
 
+  // Decrements `value_` by one, underflowing to the maximum value if necessary.
   CyclicInt &operator--() {
     --value_;
     if (value_ < min_) value_ = max_;
