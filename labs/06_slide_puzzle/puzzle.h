4e7786b36714e9af365dd5a57c96b7168cc402eb
DavidLawrence25 <25dlawrence@go.dsdmail.net>
2023-10-12 10:10:17 -0600

    Created 06_slide_puzzle/puzzle.h

---
+++ labs/06_slide_puzzle/puzzle.h
@@ -0,0 +1,37 @@
+#include <random>
+#include <vector>
+
+#ifndef CS1410_LABS_06SLIDEPUZZLE_PUZZLE_H_
+#define CS1410_LABS_06SLIDEPUZZLE_PUZZLE_H_
+
+namespace rose {
+
+// Represents a slide puzzle.
+class Puzzle {
+ public:
+  Puzzle(int size);
+  Puzzle(std::vector<std::vector<int>> tiles);
+  Puzzle(int size, std::vector<std::vector<int>> tiles);
+  Puzzle(Puzzle &other);
+
+  std::vector<int> get_position_of(int value);
+
+  // Returns true if a tile exists in the puzzle with the given `value`.
+  bool TileExists(int value);
+  // Returns true if the tile at `position` is adjacent to an empty tile.
+  bool CanMoveTile(std::vector<int> position);
+  // Returns true if a tile with the given `value` is adjacent to an empty tile.
+  bool CanMoveTile(int value);
+  // Swaps the values of two tiles, specified by `position0` and `position1`.
+  void SwapTiles(std::vector<int> position0, std::vector<int> position1);
+  // Shuffles the puzzle with `moves` number of moves.
+  void Shuffle(std::mt19937 rng, int moves);
+
+ private:
+  int size_;
+  std::vector<std::vector<int>> tiles_;
+};
+
+}
+
+#endif  // CS1410_LABS_06SLIDEPUZZLE_PUZZLE_H_
